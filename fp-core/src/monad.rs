a98cdf060b3ec01f20d6214ca8f555d3d054bbcf
Heman Gandhi <hemangandhi@gmail.com>
2019-12-27 00:14:30 -0500

    started macro

--- fp-core/src/monad.rs
+++ fp-core/src/monad.rs
@@ -6,3 +6,9 @@ pub trait Monad<A, B>: Chain<B> + Applicative<A, B> {}
 impl<A, B> Monad<A, B> for Option<A> {}
 
 impl<A, B, E> Monad<A, B> for Result<A, E> {}
+
+macro_rules! mon_do {
+    ($e: expr; $($rest:tt)*) => $e.chain(|_| mon_do!($($rest)*)),
+    (return<$hkt:ty of $s:ty> $e:expr;) => <$t<$s> as Pure<$s>>::of($e),
+
+}
